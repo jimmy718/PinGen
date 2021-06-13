7d3d66f88e99dcc097d8cb99ff502650f534dc6b
James <james.ibberson@elder-studios.co.uk>
2021-06-13 15:16:02 +0100

    Add tests for ording of results and flag reset

--- app/Http/Controllers/PINController.php
+++ app/Http/Controllers/PINController.php
@@ -10,12 +10,17 @@ class PINController extends Controller
     public function index(Request $request)
     {
         $pins = PIN::query()
+            ->inRandomOrder()
             ->limit($request->input('count', 5))
             ->get();
 
         $pins->each(function (PIN $pin) {
             $pin->markUsed();
         });
+
+        if (PIN::query()->where('used', false)->count() === 0) {
+            PIN::where('used', true)->update(['used' => false]);
+        }
 
         return $pins;
     }
--- tests/Feature/ListPINsTest.php
+++ tests/Feature/ListPINsTest.php
@@ -32,11 +32,48 @@ class ListPINsTest extends TestCase
         $response = $this->getJson(route('PINs.index', ['count' => 3]))->assertJsonCount(3);
 
         $returnedPINs = $response->original;
-
-        $returnedPINs->map->used->each(function ($flag) {
+        $returnedPINs->pluck('used')->each(function ($flag) {
             $this->assertTrue($flag);
         });
 
         $this->assertCount(1, PIN::where('used', false)->get());
+    }
+
+    /** @test */
+    public function PINs_are_returned_in_a_random_order()
+    {
+        PIN::create(['value' => '1356']);
+        PIN::create(['value' => '1357']);
+        PIN::create(['value' => '1358']);
+
+        $response = $this->getJson(route('PINs.index', ['count' => 3]))->assertJsonCount(3);
+
+        $returnedPINs = $response->original;
+        $nextId = $returnedPINs->first()->id + 1;
+        $countOfConsecutiveIds = 0;
+
+        for ($i = 1; $i < $returnedPINs->count(); $i++) {
+            if ($returnedPINs[$i]->id === $nextId) {
+                $countOfConsecutiveIds++;
+            }
+        }
+
+        $this->assertTrue(
+            $countOfConsecutiveIds <= 2,
+            'PINs where returned with more than 2 consecutive IDs'
+        );
+    }
+
+    /** @test */
+    public function when_all_PINs_are_exhausted_all_used_markers_are_reset()
+    {
+        PIN::create(['value' => '1356']);
+        PIN::create(['value' => '1357']);
+        PIN::create(['value' => '1358']);
+
+        $this->getJson(route('PINs.index', ['count' => 3]))->assertJsonCount(3);
+
+        // at this point all PINs have been used and "used" flags should have been reset
+        $this->assertCount(3, PIN::where('used', false)->get());
     }
 }
